834ad04e3e227e1c0f8ec0d88a7a061d29c2e1ac
Jesus-Silva <jesus.silvap@gmail.com>
2023-07-25 21:42:18 -0500

    se agrego texto del ejercicio 3

--- Javascript/Ejercicio2/js/script2.js
+++ Javascript/Ejercicio2/js/script2.js
@@ -1,9 +1,13 @@
-/* Dado el sueldo de un empleado
+/* Ejercicio 2- Dado el sueldo de un empleado
 imprimir en una alerta el nuevo sueldo
-del empleado si tuvo un aumento del 10% */
+del empleado si tuvo un aumento del 10% 
 
 const sueldo = prompt("Ingrese el valor del sueldo");
 const psueldo= 10/100;
 const nsueldo = parseInt(sueldo) + parseInt(sueldo*psueldo);
-alert("el nuevo sueldo con el aumento es: "+nsueldo);
+alert("el nuevo sueldo con el aumento es: "+nsueldo); */
 
+/* Ejercicio 3 - Un vendedor que tiene su sueldo base,
+recibe una comision del 30% del total de las ventas 
+del mes, el quiere saber cuanto ganara en un mes que
+tuvo tres ventas*/
