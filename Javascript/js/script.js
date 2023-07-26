2f21b577e4921551ebba030bccadc68ea5f22f9a
Jesus-Silva <jesus.silvap@gmail.com>
2023-07-25 19:17:53 -0500

    Agregando carpeta de javascript

---
+++ Javascript/js/script.js
@@ -0,0 +1,54 @@
+//console.log("Analisis y Desarrollo de Software");
+
+//alert("Analisis y Desarrollo de Software");
+
+//para comentar en una linea de codigo
+
+/* este tipo de comentario
+se trabaja en diferentes lineas 
+de codigo */
+
+//document.write("Analisis y Desarrollo de Software");
+
+//declarar variables var 
+
+//var edad=33;
+//const nom="Jesus";
+//let num=2.5;
+//booleano='ADSO';
+
+/* unir dos cadenas de texto
+const nom="Jesus"
+const ape='Silva'
+alert("mi nombre es "+nom+" y mi apellido es "+ape) */
+
+/* operadores aritmeticos 
+const num1=5
+const num2=3
+const div= num1/num2
+alert("la division es: "+div)*/
+
+let nom=prompt("Escriba nombre")
+let ape=prompt("Escriba apellido")
+alert("mi nombre es: "+nom+" "+ape)
+
+/*mostrar en una alerta los nombres y apellidos
+completos, edad, peso y direccion utilizando 
+la funcion prompt*/
+
+
+
+
+
+
+//console.log(typeof booleano); typeof es para definir el tipo de dato
+
+//alert("Mi edad es: "+edad);
+//document.write("Mi edad es: "+edad);
+// simbolo "+" va funcionar para concatenar-unir
+
+
+//const edad2=22;
+
+//let edad3=33;
+
